461b3d687bbde22b22a706ba5c991cf22ae0d439
sordonez <sordonez@servicios-it.dga.gob.ni:10000>
2021-02-02 10:41:15 -0600

    added fetch statistics on statistic route

--- src/pages/Statistic.js
+++ src/pages/Statistic.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import {
+  ListGroup,
+  ListGroupItem,
+  ListGroupItemHeading,
+  ListGroupItemText,
+} from 'reactstrap';
+import { FetchContext } from '../context/FetchContext';
 
-const Statistict = () => {
+const StatisticItem = ({ stat }) => {
   return (
     <>
-      <h3>Country Statistics</h3>
+      <ListGroupItem>
+        <ListGroupItemHeading>{stat.country}</ListGroupItemHeading>
+        <ListGroupItemText>
+          {stat.continent} - Total Population {stat.population}
+        </ListGroupItemText>
+      </ListGroupItem>
+    </>
+  );
+};
+
+const StatisticItemContainer = ({ statistics }) => {
+  return (
+    <ListGroup>
+      {statistics && statistics.length
+        ? statistics.map(stat => <StatisticItem key={stat._id} stat={stat} />)
+        : 'No Inventory Items'}
+    </ListGroup>
+  );
+};
+
+const Statistict = () => {
+  const fetchContext = useContext(FetchContext);
+  const [statistics, setStatistics] = useState([]);
+
+  useEffect(() => {
+    const getStatisticts = async () => {
+      try {
+        const { data } = await fetchContext.authAxios.get('/statistics');
+        setStatistics(data);
+      } catch (err) {
+        console.log('the err', err);
+      }
+    };
+
+    getStatisticts();
+  }, [fetchContext]);
+
+  return (
+    <>
+      <StatisticItemContainer statistics={statistics} />
     </>
   );
 };
